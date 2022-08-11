fe80114b86783c592e421d88382a8e23ae4d8b1c
lijun <lij@jjcclife.com>
2022-08-11 16:26:25 +0800

    init

--- internal/cron/cron.go
+++ internal/cron/cron.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gogf/gf/v2/os/gtime"
+
 	"github.com/gogf/gf/v2/os/gcron"
 )
 
 func Schedule(ctx context.Context) {
 	_, errCron := gcron.Add(ctx, "*/5 * * * * *", func(ctx context.Context) {
-		fmt.Println("Every five second")
+		fmt.Println(gtime.Datetime() + " Every five second")
 	}, "MySecondCronJob")
 	if errCron != nil {
 		panic(errCron)
